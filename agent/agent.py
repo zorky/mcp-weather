33aa43f202a86738c55d5184bf7982a228d5921b
Olivier DUVAL <zorky00@gmail.com>
2025-08-16 17:16:56 +0200

    ajout tools pour l agent

--- agent/agent.py
+++ agent/agent.py
@@ -3,6 +3,8 @@ from langchain.agents import initialize_agent, AgentType
 from langchain_community.chat_models import ChatOpenAI
 
 from tools.crypto_price import get_crypto_price
+from tools.holidays import get_jours_feries, get_vacances_scolaires
+from tools.search_web import duckduckgo_search
 from tools.weather_tools import get_weather
 from tools.geo_tools import get_coordinates_openmeteo
 
@@ -12,7 +14,11 @@ LLM_API=os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1")
 LLM_TEMPERATURE=0.3  # 0 : déterministe et précis, 0.3 : un peu plus créatif, etc
 
 # tools = [get_weather, get_crypto_price]
-tools = [get_weather, get_crypto_price, get_coordinates_openmeteo]
+tools = [get_weather,
+         get_crypto_price,
+         get_coordinates_openmeteo,
+         get_jours_feries, get_vacances_scolaires,
+         duckduckgo_search]
 # tools = [get_weather, get_coordinates_openstreetmap]
 # tools = [get_coordinates_openmeteo, get_weather]
 
