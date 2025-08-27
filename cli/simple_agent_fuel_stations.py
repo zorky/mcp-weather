bbeca2589ca39f242e4a532906a60da46ba39eae
Olivier Duval <olivier.duval@univ-paris8.fr>
2025-08-27 10:10:54 +0200

    +oxylabs account free trial

--- cli/simple_agent_fuel_stations.py
+++ cli/simple_agent_fuel_stations.py
@@ -4,6 +4,9 @@ https://ai.plainenglish.io/building-an-ai-agent-with-agno-a-step-by-step-guide-1
 https://github.com/Bavalpreet/AI-Agents/tree/main/agno
 
 API : https://developers.oxylabs.io/scraping-solutions/web-scraper-api/targets/google/search/local-search
+Free trial : https://dashboard.oxylabs.io/en/overview/scraper/buy-now
+
+curl ''https://realtime.oxylabs.io/v1/queries'' --user 'zorky_magBl:***************' -H 'Content-Type: application/json' -d '{"source": "google_search", "query": "adidas", "geo_location": "California,United States", "parse": true}'
 """
 
 import os
@@ -13,6 +16,9 @@ from agno.agent import Agent
 
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from dotenv import load_dotenv
+load_dotenv()
 
 from cli.ollama_client_model import get_ollama_model
 
@@ -26,19 +32,21 @@ class FuelStationSearchTool:
     A tool for searching fuel stations using the Oxylabs API.
     https://developers.oxylabs.io/scraping-solutions/web-scraper-api/targets/google/search/local-search
     """
-    USERNAME="baval_6hy9U"
-    PASSWORD="Baval=123456789"
+    USERNAME=os.getenv('LOGIN')    
+    PASSWORD=os.getenv('PASSWORD')
+    
     URL="https://realtime.oxylabs.io/v1/queries"
 
     def __init__(self):
         self.name = "FuelStationSearchTool"
         self.description = "Fetches nearby fuel stations using Oxylabs Real-Time API."
 
-    def run(self, location_query, pages=1):
+    def run(self, location_query, pages=1):    
+        logger.debug(f"usr / pwd : {self.USERNAME} {self.PASSWORD}")
         payload = {
             'source': 'google_maps',
             'domain': 'fr',
-            'query': f'fuel stations in {location_query}',
+            'query': f'stations services à {location_query}',
             'pages': pages,
         }
         logger.debug(f"Oxylabs API payload: {payload} with {self.URL}")
@@ -53,13 +61,15 @@ class FuelStationSearchTool:
 
         data = response.json()
 
-        stations_info = []
-        logger.debug(f"Oxylabs API response data: {data}")
+        stations_info = []        
+        logger.debug(f"results : {data.get('results')[:1]}")
         for result in data.get('results', [])[:5]:
             title = result.get('title', 'No Name')
             address = result.get('address', 'Address Unavailable')
             rating = result.get('rating', 'Rating Unavailable')
-            stations_info.append(f"{title} - {address} (Rating: {rating})")
+            station_data = f"{title} - {address} (Rating: {rating})"
+            stations_info.append(station_data)
+            logger.debug(f"Oxylabs API response data: {station_data}")
 
         if not stations_info:
             return "No nearby fuel stations found."
@@ -85,8 +95,10 @@ agent = Agent(
 
 # Main execution
 if __name__ == "__main__":
-    default_location="Noisy-le-Grand, France"
-    location = input(f"Entrez votre localisation (ou appuyez sur Entrée pour la question par défaut '{default_location}') : ") or default_location
-    # location_query = "NLS, Missisauga, ON"    
-    location_query = location
-    agent.print_response(location_query)
+    fuel = FuelStationSearchTool()
+    fuel.run(location_query="Noisy-le-Grand, France")
+
+    # default_location="Noisy-le-Grand, France"
+    # location = input(f"Entrez votre localisation (ou appuyez sur Entrée pour la question par défaut '{default_location}') : ") or default_location    
+    # location_query = location
+    # agent.print_response(location_query)
